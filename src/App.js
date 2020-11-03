adda83ebb534c5951242621dc2a95cb273576661
Sebastián Solari <35378033+ssolaric@users.noreply.github.com>
2020-11-03 10:20:50 -0500

    Remove bulma css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import "bulma/css/bulma.css";
-
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
@@ -27,114 +25,60 @@ function App() {
   };
 
   return (
-    <div className="container">
-      <div className="columns is-centered">
-        <div className="column is-half">
-          <h1 className="title">Random Array Generator</h1>
-          <form action="" onSubmit={handleSubmit(onSubmit)}>
-            <div className="field">
-              <label htmlFor="array-size" class="label">
-                Array size
-              </label>
-              <div className="control">
-                <input
-                  ref={register}
-                  type="number"
-                  name="arraySize"
-                  id="array-size"
-                  className="input"
-                />
-              </div>
-              <p className="has-text-danger">{errors.arraySize?.message}</p>
-            </div>
-            <div className="field">
-              <label htmlFor="minimum-value" class="label">
-                Minimum value
-              </label>
-              <div className="control">
-                <input
-                  ref={register}
-                  type="number"
-                  name="minimumValue"
-                  id="minimum-value"
-                  className="input"
-                />
-              </div>
-              <p className="has-text-danger">{errors.minimumValue?.message}</p>
-            </div>
-            <div className="field">
-              <label htmlFor="maximum-value" class="label">
-                Maximum value
-              </label>
-              <div className="control">
-                <input
-                  ref={register}
-                  type="number"
-                  name="maximumValue"
-                  id="maximum-value"
-                  className="input"
-                />
-              </div>
-              <p className="has-text-danger">{errors.maximumValue?.message}</p>
-            </div>
-            <div className="field">
-              <label htmlFor="allow-repeated" class="label">
-                Allow repeated elements?
-              </label>
-              <input
-                ref={register}
-                type="checkbox"
-                name="allowRepeated"
-                id="allow-repeated"
-                className="checkbox"
-              />
-              <p className="has-text-danger">{errors.allowRepeated?.message}</p>
-            </div>
+    <div>
+      <h1>Random Array Generator</h1>
+      <form action="" onSubmit={handleSubmit(onSubmit)}>
+        <label htmlFor="array-size">Array size</label>
 
-            <div className="control">
-              <button type="submit" className="button is-primary">
-                Submit
-              </button>
-            </div>
-          </form>
+        <input ref={register} type="number" name="arraySize" id="array-size" />
+        <p>{errors.arraySize?.message}</p>
 
-          <div className="field">
-            <label htmlFor="plain-text">Plain text</label>
-            <div className="control">
-              <textarea
-                id="plain-text"
-                value={outputPlainText}
-                readOnly
-                class="textarea"
-              ></textarea>
-            </div>
-          </div>
+        <label htmlFor="minimum-value">Minimum value</label>
 
-          <div className="field">
-            <label htmlFor="curly-brackets">Curly brackets (C++ style)</label>
-            <div className="control">
-              <textarea
-                value={outputCurlyBrackets}
-                readOnly
-                class="textarea"
-              ></textarea>
-            </div>
-          </div>
+        <input
+          ref={register}
+          type="number"
+          name="minimumValue"
+          id="minimum-value"
+        />
+        <p>{errors.minimumValue?.message}</p>
 
-          <div className="field">
-            <label htmlFor="square-brackets">
-              Square brackets (JS/Python style)
-            </label>
-            <div className="control">
-              <textarea
-                value={outputSquareBrackets}
-                readOnly
-                class="textarea"
-              ></textarea>
-            </div>
-          </div>
-        </div>
-      </div>
+        <label htmlFor="maximum-value">Maximum value</label>
+
+        <input
+          ref={register}
+          type="number"
+          name="maximumValue"
+          id="maximum-value"
+        />
+        <p>{errors.maximumValue?.message}</p>
+
+        <label htmlFor="allow-repeated">Allow repeated elements?</label>
+        <input
+          ref={register}
+          type="checkbox"
+          name="allowRepeated"
+          id="allow-repeated"
+        />
+        <p>{errors.allowRepeated?.message}</p>
+
+        <button type="submit">Submit</button>
+      </form>
+
+      <p>Plain text</p>
+      <pre>
+        <code>{outputPlainText}</code>
+      </pre>
+
+      <p>Curly brackets (C++ style)</p>
+      <pre>
+        <code>{outputCurlyBrackets}</code>
+      </pre>
+
+      <p>Square brackets (JS/Python style)</p>
+      <pre>
+        <code>{outputSquareBrackets}</code>
+      </pre>
     </div>
   );
 }
