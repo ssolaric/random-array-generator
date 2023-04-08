173d2a804e1f8d406a893b8e83b7da05693f790f
Sebastián Solari <35378033+ssolaric@users.noreply.github.com>
2023-04-08 00:44:40 -0500

    Add Daisy UI

--- src/App.tsx
+++ src/App.tsx
@@ -23,48 +23,68 @@ function App() {
   };
 
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <label>
-          Array size
-          <input
-            type="number"
-            {...register("arraySize", { valueAsNumber: true })}
-          />
-        </label>
-        <p>{errors.arraySize?.message}</p>
-        <label>
-          Minimum value
-          <input
-            type="number"
-            {...register("minimumValue", { valueAsNumber: true })}
-          />
-        </label>
-        <p>{errors.minimumValue?.message}</p>
-        <label>
-          Maximum value
-          <input
-            type="number"
-            {...register("maximumValue", { valueAsNumber: true })}
-          />
-        </label>
-        <p>{errors.maximumValue?.message}</p>
-        <label>
-          Allow repeated elements
-          <input type="checkbox" {...register("allowRepeated")} />
-        </label>
-        <p>{errors.allowRepeated?.message}</p>
-        <button type="submit">Submit</button>
-      </form>
+    <div className="w-full flex items-center justify-center h-screen bg-green-50">
+      {/* <div className="w-full sm:max-w-lg shadow-lg p-4 bg-white"> */}
+      <div className="w-full md:max-w-xl shadow-lg p-8 bg-white">
+        <h1 className="text-2xl font-bold text-center">Random Array Generator</h1>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="form-control">
+            <label className="label label-text" htmlFor="arraySize">
+              <span className="label-text">Array size</span>
+            </label>
+            <input
+              className="input input-bordered input-primary"
+              type="number"
+              {...register("arraySize", { valueAsNumber: true })}
+            />
+            <p className="text-error">{errors.arraySize?.message}</p>
+          </div>
+          <div className="form-control">
+            <label className="label label-text" htmlFor="minimumValue">
+              Minimum value
+            </label>
+            <input
+              className="input input-bordered input-primary"
+              type="number"
+              {...register("minimumValue", { valueAsNumber: true })}
+            />
+            <p className="text-error">{errors.minimumValue?.message}</p>
+          </div>
+          <div className="form-control">
+            <label className="label label-text" htmlFor="maximumValue">
+              Maximum value
+            </label>
+            <input
+              className="input input-bordered input-primary"
+              type="number"
+              {...register("maximumValue", { valueAsNumber: true })}
+            />
+            <p className="text-error">{errors.maximumValue?.message}</p>
+          </div>
+          <div className="form-control">
+            <label className="label label-text" htmlFor="allowRepeated">
+              Allow repeated elements
+              <input
+                className="checkbox checkbox-primary"
+                type="checkbox"
+                {...register("allowRepeated")}
+              />
+            </label>
+            <p className="text-error">{errors.allowRepeated?.message}</p>
+          </div>
+          <button className="btn btn-primary" type="submit">
+            Submit
+          </button>
+        </form>
 
-      {isValid && isSubmitSuccessful && (
-        <div>
-          <p>Plain text</p>
-          <pre>{output}</pre>
-          <button>Copy to clipboard</button>
-        </div>
-      )}
+        {isValid && isSubmitSuccessful && (
+          <div>
+            <p>Plain text</p>
+            <pre>{output}</pre>
+            <button>Copy to clipboard</button>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
